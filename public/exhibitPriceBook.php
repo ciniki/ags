5eb2e3b5c75bf6f7c6fdefee0b0e950dcd674249
Andrew Rivett <veggiefrog@gmail.com>
2018-08-13 14:29:16 -0400

    Added price book pdf

---
+++ public/exhibitPriceBook.php
@@ -0,0 +1,167 @@
+<?php
+//
+// Description
+// ===========
+// This method will generate a PDF of the prices organized by Type for an exhibit.
+// By default only items that are untagged will be included.
+//
+// Arguments
+// ---------
+// api_key:
+// auth_token:
+// tnid:     The ID of the tenant the seller is attached to.
+// exhibit_id:       The ID of the exhibition to get the details for.
+// 
+// Returns
+// -------
+//
+function ciniki_ags_exhibitPriceBook($ciniki) {
+    //  
+    // Find all the required and optional arguments
+    //  
+    ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'prepareArgs');
+    $rc = ciniki_core_prepareArgs($ciniki, 'no', array(
+        'tnid'=>array('required'=>'yes', 'blank'=>'no', 'name'=>'Tenant'), 
+        'exhibit_id'=>array('required'=>'yes', 'blank'=>'no', 'name'=>'Exhibit'),
+        'participant_id'=>array('required'=>'no', 'blank'=>'yes', 'name'=>'Seller'),
+        'tagged'=>array('required'=>'no', 'blank'=>'yes', 'name'=>'Included Tagged Items'),
+        'output'=>array('required'=>'no', 'blank'=>'no', 'name'=>'Format'),
+        )); 
+    if( $rc['stat'] != 'ok' ) { 
+        return $rc;
+    }   
+    $args = $rc['args'];
+    
+    //  
+    // Make sure this module is activated, and
+    // check permission to run this function for this tenant
+    //  
+    ciniki_core_loadMethod($ciniki, 'ciniki', 'ags', 'private', 'checkAccess');
+    $rc = ciniki_ags_checkAccess($ciniki, $args['tnid'], 'ciniki.ags.exhibitPriceList'); 
+    if( $rc['stat'] != 'ok' ) { 
+        return $rc;
+    }   
+    $modules = $rc['modules'];
+
+    //
+    // Load the tenant intl settings
+    //
+    ciniki_core_loadMethod($ciniki, 'ciniki', 'tenants', 'private', 'intlSettings');
+    $rc = ciniki_tenants_intlSettings($ciniki, $args['tnid']);
+    if( $rc['stat'] != 'ok' ) {
+        return $rc;
+    }
+    $intl_timezone = $rc['settings']['intl-default-timezone'];
+    $intl_currency_fmt = numfmt_create($rc['settings']['intl-default-locale'], NumberFormatter::CURRENCY);
+    $intl_currency = $rc['settings']['intl-default-currency'];
+
+    ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'dbQuote');
+    ciniki_core_loadMethod($ciniki, 'ciniki', 'users', 'private', 'dateFormat');
+    $date_format = ciniki_users_dateFormat($ciniki);
+
+    //
+    // Load tenant details
+    //
+    ciniki_core_loadMethod($ciniki, 'ciniki', 'tenants', 'private', 'tenantDetails');
+    $rc = ciniki_tenants_tenantDetails($ciniki, $args['tnid']);
+    if( $rc['stat'] != 'ok' ) {
+        return $rc;
+    }
+    if( isset($rc['details']) && is_array($rc['details']) ) {
+        $tenant_details = $rc['details'];
+    } else {
+        $tenant_details = array();
+    }
+
+    //
+    // Get the exhibit name
+    //
+    $strsql = "SELECT name "
+        . "FROM ciniki_ags_exhibits "
+        . "WHERE id = '" . ciniki_core_dbQuote($ciniki, $args['exhibit_id']) . "' "
+        . "AND tnid = '" . ciniki_core_dbQuote($ciniki, $args['tnid']) . "' "
+        . "";
+    $rc = ciniki_core_dbHashQuery($ciniki, $strsql, 'ciniki.ags', 'exhibition');
+    if( $rc['stat'] != 'ok' ) {
+        return $rc;
+    }
+    if( !isset($rc['exhibition']) ) {
+        return array('stat'=>'fail', 'err'=>array('code'=>'ciniki.ags.22', 'msg'=>'Exhibition does not exist'));
+    }
+    $exhibit_name = $rc['exhibition']['name'];
+
+    //
+    // Get the list of exhibit items, sorted by type
+    //
+    $strsql = "SELECT ciniki_ags_exhibit_items.id, "
+        . "items.id AS item_id, "
+        . "items.code, "
+        . "IFNULL(ciniki_ags_exhibitors.display_name, '') AS display_name, "
+        . "items.name, "
+        . "items.flags, "
+        . "items.unit_amount, "
+        . "IFNULL(tags.tag_name, 'Misc') AS type "
+        . "FROM ciniki_ags_exhibit_items "
+        . "INNER JOIN ciniki_ags_items AS items ON ("
+            . "ciniki_ags_exhibit_items.item_id = items.id "
+            . "AND items.tnid = '" . ciniki_core_dbQuote($ciniki, $args['tnid']) . "' "
+            . ") "
+        . "LEFT JOIN ciniki_ags_exhibitors ON ("
+            . "items.exhibitor_id = ciniki_ags_exhibitors.id "
+            . "AND ciniki_ags_exhibitors.tnid = '" . ciniki_core_dbQuote($ciniki, $args['tnid']) . "' "
+            . ") "
+        . "LEFT JOIN ciniki_ags_item_tags AS tags ON ("
+            . "items.id = tags.item_id "
+            . "AND tags.tag_type = 10 "
+            . "AND tags.tnid = '" . ciniki_core_dbQuote($ciniki, $args['tnid']) . "' "
+            . ") "
+        . "WHERE ciniki_ags_exhibit_items.tnid = '" . ciniki_core_dbQuote($ciniki, $args['tnid']) . "' "
+        . "AND ciniki_ags_exhibit_items.exhibit_id = '" . ciniki_core_dbQuote($ciniki, $args['exhibit_id']) . "' "
+        . "";
+    if( !isset($args['tagged']) || $args['tagged'] != 'yes' ) {
+        $strsql .= "AND (items.flags&0x10) = 0 ";
+    }
+    $strsql .= "ORDER BY type, code, name "
+        . "";
+    ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'dbHashQueryIDTree');
+    $rc = ciniki_core_dbHashQueryIDTree($ciniki, $strsql, 'ciniki.ags', array(
+        array('container'=>'types', 'fname'=>'type',
+            'fields'=>array('name'=>'type'),
+            ),
+        array('container'=>'items', 'fname'=>'id',
+            'fields'=>array('id', 'display_name', 'code', 'name', 'flags', 'unit_amount'),
+            ),
+        ));
+    if( $rc['stat'] != 'ok' ) {
+        return $rc;
+    }
+    if( !isset($rc['types']) ) {
+        $types = array();   
+    } else {
+        $types = $rc['types'];
+    }
+
+    $today = new DateTime('now', new DateTimeZone($intl_timezone));
+
+    ciniki_core_loadMethod($ciniki, 'ciniki', 'ags', 'templates', 'pricebook');
+    $rc = ciniki_ags_templates_pricebook($ciniki, $args['tnid'], array(
+        'title'=>$exhibit_name . ' - Price Book',
+        'author'=>$tenant_details['name'],
+        'footer'=>$today->format('M d, Y'),
+        'types'=>$types,
+        ));
+    if( $rc['stat'] != 'ok' ) { 
+        return $rc;
+    }
+    $pdf = $rc['pdf'];
+
+    //
+    // Output the pdf
+    //
+    $filename = $exhibit_name . ' - Price Book - ' . $today->format('M d, Y');
+    $filename = preg_replace('/[^A-Za-z0-9\-]/', '', $filename) . '.pdf';
+    $pdf->Output($filename, 'D');
+
+    return array('stat'=>'exit');
+}
+?>
---
+++ templates/pricebook.php
@@ -0,0 +1,184 @@
+<?php
+//
+// Description
+// ===========
+// This function returns a PDF of the price list for a market.
+//
+// Arguments
+// ---------
+// 
+// Returns
+// -------
+//
+function ciniki_ags_templates_pricebook(&$ciniki, $tnid, $args) {
+
+    //
+    // Load TCPDF library
+    //
+    require_once($ciniki['config']['ciniki.core']['lib_dir'] . '/tcpdf/tcpdf.php');
+
+    class MYPDF extends TCPDF {
+        //Page header
+        public $title = '';
+        public $footer_msg = '';
+
+        public function Header() {
+            //
+            // Output the title
+            //
+            $this->SetFont('', 'B', 14);
+            $this->Cell(180, 12, $this->title, 0, false, 'C', 0);
+        }
+
+        // Page footer
+        public function Footer() {
+            // Position at 15 mm from bottom
+            $this->SetY(-15);
+            $this->SetFont('helvetica', 'I', 8);
+            if( isset($this->footer_msg) && $this->footer_msg != '' ) {
+                $this->Cell(90, 10, $this->footer_msg, 0, false, 'L', 0, '', 0, false, 'T', 'M');
+                $this->Cell(90, 10, 'Page ' . $this->pageNo().'/'.$this->getAliasNbPages(), 0, false, 'R', 0, '', 0, false, 'T', 'M');
+            } else {
+                // Center the page number if no footer message.
+                $this->Cell(0, 10, 'Page ' . $this->pageNo().'/'.$this->getAliasNbPages(), 0, false, 'C', 0, '', 0, false, 'T', 'M');
+            }
+        }
+    }
+
+    //
+    // Start a new document
+    //
+    $pdf = new MYPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+
+    //
+    // Figure out the header tenant name and address information
+    //
+    $pdf->header_height = 20;
+    $pdf->header_height = 30;
+    if( isset($args['title']) ) {
+        $pdf->title = $args['title'];
+    }
+    if( isset($args['footer']) ) {
+        $pdf->footer_msg = $args['footer'];
+    }
+
+    //
+    // Setup the PDF basics
+    //
+    $pdf->SetCreator('Ciniki');
+    $pdf->SetAuthor($args['author']);
+    $pdf->SetTitle($pdf->title);
+    $pdf->SetSubject('');
+    $pdf->SetKeywords('');
+
+    // set margins
+    $pdf->SetMargins(PDF_MARGIN_LEFT, 19, PDF_MARGIN_RIGHT);
+    $pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
+    $pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
+
+    // set font
+    $pdf->SetFont('times', 'BI', 10);
+    $pdf->SetCellPadding(2, 1.5, 2, 1.5);
+
+    // add a page
+    $pdf->SetFillColor(255);
+    $pdf->SetTextColor(0);
+    $pdf->SetDrawColor(51);
+    $pdf->SetLineWidth(0.15);
+
+    // Set barcode style
+    $style = array(
+        'position' => '',
+        'align' => 'C',
+        'stretch' => false,
+        'fitwidth' => true,
+        'cellfitalign' => '',
+        'border' => false,
+        'hpadding' => 'auto',
+        'vpadding' => 'auto',
+        'fgcolor' => array(0,0,0),
+        'bgcolor' => false,
+        'text' => true,
+        'font' => 'helvetica',
+        'fontsize' => 7,
+        'stretchtext' => 4,
+        );
+
+    //
+    // Add the items
+    //
+    $w = array(35, 80, 45, 20);
+
+    $fill=0;
+    $lh = 8;
+    foreach($args['types'] as $itemtype) {
+
+        $pdf->AddPage();
+        $pdf->SetFont('', 'B', 16);
+        $pdf->Cell(180, 12, $itemtype['name'], 0, 0, 'L', 0);
+        $pdf->SetFont('', 'B', 10);
+        $pdf->Ln(12);
+        $pdf->SetFillColor(224);
+        $pdf->SetFont('', 'B');
+        $pdf->SetCellPadding(2);
+        $pdf->Cell($w[0], 10, 'Barcode', 1, 0, 'C', 1);
+        $pdf->Cell($w[1], 10, 'Item', 1, 0, 'C', 1);
+        $pdf->Cell($w[2], 10, 'Exhibitor', 1, 0, 'C', 1);
+        $pdf->Cell($w[3], 10, 'Price', 1, 0, 'C', 1);
+        $pdf->Ln(10);
+        $pdf->SetFillColor(236);
+        $pdf->SetTextColor(0);
+        $pdf->SetFont('');
+
+        foreach($itemtype['items'] as $item) {
+            $code = $item['code'];
+            $name = $item['name'];
+            if( ($item['flags']&0x01) == 0 ) {
+                $price = 'NFS';
+            } else {
+                $price = '$' . number_format($item['unit_amount'], 2);
+            }
+            $nlines = $pdf->getNumLines($name, $w[1]);
+            if( $nlines == 2 ) {
+                $lh = 3+($nlines*5);
+            } elseif( $nlines > 2 ) {
+                $lh = 2+($nlines*5);
+            } else {
+                $lh = 10;
+            }
+            // Check if we need a page break
+            if( $pdf->getY() > ($pdf->getPageHeight() - 26) ) {
+                $pdf->AddPage();
+                $pdf->SetFont('', 'B', 16);
+                $pdf->Cell(180, 12, $itemtype['name'] . ' (continued)', 0, 0, 'L', 0);
+                $pdf->SetFont('', 'B', 10);
+                $pdf->Ln(12);
+                $pdf->SetFillColor(224);
+                $pdf->Cell($w[0], 10, 'Barcode', 1, 0, 'C', 1);
+                $pdf->Cell($w[1], 10, 'Item', 1, 0, 'C', 1);
+                $pdf->Cell($w[2], 10, 'Exhibitor', 1, 0, 'C', 1);
+                $pdf->Cell($w[3], 10, 'Price', 1, 0, 'C', 1);
+                $pdf->Ln(10);
+                $pdf->SetFillColor(236);
+                $pdf->SetTextColor(0);
+                $pdf->SetFont('');
+            }
+            $x = $pdf->getX();
+            $y = $pdf->getY();
+            $pdf->MultiCell($w[0], $lh, '', 1, 'L', $fill, 0, '', '', true, 0, false, true, 0, 'T', false);
+            $pdf->MultiCell($w[1], $lh, $name, 1, 'L', $fill, 0, '', '', true, 0, false, true, 0, 'T', false);
+            $pdf->MultiCell($w[2], $lh, $item['display_name'], 1, 'L', $fill, 0, '', '', true, 0, false, true, 0, 'T', false);
+            $pdf->MultiCell($w[3], $lh, $price, 1, 'R', $fill, 0, '', '', true, 0, false, true, 0, 'T', false);
+            $pdf->Ln(); 
+            $x2 = $pdf->getX();
+            $y2 = $pdf->getY();
+            $pdf->write1DBarcode($code, 'C39', $x, $y+0.25, $w[0], 14, 0.3, $style, 'C');
+            $pdf->SetX($x2);
+            $pdf->SetY($y2);
+            $fill=!$fill;
+        }
+    }
+
+    return array('stat'=>'ok', 'pdf'=>$pdf);
+}
+?>
